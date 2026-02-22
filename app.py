a7bff565184435fb959c2380eab6c12ad33dd330
kamkei040-bit <kamkei040@gmail.com>
2026-02-22 15:50:24 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-from flask import Flask
+from flask import Flask, request
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route("/")
+def home():
+    return "LINE Bot is running"
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    data = request.json
+    print("Received from LINE:", data)
+    return "OK"
+
+if __name__ == "__main__":
+    app.run()
